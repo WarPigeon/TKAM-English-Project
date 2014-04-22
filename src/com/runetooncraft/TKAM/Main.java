c812bb87c9fcd563e7c935caf19a47daafe9f61c
Deery50 <marcus@marcusman.com>
2014-04-22 19:37:20 -0400

    Initial Setup

--- src/com/runetooncraft/TKAM/Main.java
+++ src/com/runetooncraft/TKAM/Main.java
@@ -1,18 +1,61 @@
 package com.runetooncraft.TKAM;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 import com.runetooncraft.warpigeon.engine.GameType;
 import com.runetooncraft.warpigeon.engine.WPEngine4;
-import com.runetooncraft.warpigeon.testengine.FreeRoam;
+import com.runetooncraft.warpigeon.engine.graphics.Sprite;
+import com.runetooncraft.warpigeon.engine.utils3d.KeyListener;
+import com.runetooncraft.warpigeon.testengine.PlayerMain;
 
 
 public class Main extends WPEngine4 {
+	KeyListener KL;
 	
 	public Main(int Height, int Width, int Scale, int PixelWidth, int PixelHeight, int ImageToPixelRatio, File DataFolder) {
 		super(Height, Width, Scale, PixelWidth, PixelHeight, ImageToPixelRatio, DataFolder, GameType.FREE_ROAM_TILE_BASED);
 		DataFolder.mkdirs();
+		setIconImage();
+		SetWindowResizable(false);
+		SetWindowTitle("TKAM Game Project by Marcus Dubreuil and Ethan Defrank");
+		KL = new KeyListener();
+		SetClassInstance(this,false);
+		//level stuff here
+		setEngineKeyListener(KL);
+		Sprite[] ForwardAnims = new Sprite[2];
+		Sprite[] BackwardAnims = new Sprite[2];
+		player = new PlayerMain(KL, 0, 0, ForwardAnims, BackwardAnims, ForwardAnims, ForwardAnims);
+		player.init(level,this);
+		PackFrame();
+		start();
 	}
+	
+	private void setIconImage() {
+			try {
+				InputStream imgStream = Main.class.getResourceAsStream("/favicon.png");
+				BufferedImage myImg = ImageIO.read(imgStream);
+				GetFrame().setIconImage(myImg);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	public void update() {
+		KeyEvents.update();
+		player.update();
+	}
+	
+	public void privateRender() {
+		int xScroll = player.x + screen.width /2 - 16;
+		int yScroll = player.y + screen.height /2 - 16;
+		player.render(xScroll, yScroll, screen);
+	}
+	
 	public static void main(String[] args) {
 		String workingDirectory;
 		String OS = (System.getProperty("os.name")).toUpperCase();
